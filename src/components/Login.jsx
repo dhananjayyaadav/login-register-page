d101289b752d4daa7acc5cfd01fabae698ce39d5
Dhananjay Yadav <110413258+dhananjayyaadav@users.noreply.github.com>
2023-10-10 12:42:41 +0530

    Update Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -11,7 +11,7 @@ export default function Login() {
 
         <div className='bg-gray-800 flex flex-col justify-center'>
             <form className='max-w-[400px] w-full mx-auto rounded-lg bg-gray-900 p-8 px-8'>
-                <h2 className='text-4xl dark:text-white font-bold text-center'>SIGN IN</h2>
+                <h2 className='text-4xl dark:text-white font-bold text-center'>Already Members</h2>
                 <div className='flex flex-col text-gray-400 py-2'>
                     <label>Username</label>
                     <input className='rounded-lg bg-gray-700 mt-2 p-2 focus:border-blue-500 focus:bg-gray-800 focus:outline-none' type="text" />
